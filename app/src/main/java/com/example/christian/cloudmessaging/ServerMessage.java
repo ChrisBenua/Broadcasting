43899fd1e171da6e2d8fba20b6b51bb790c8fab0
ChrisBenua <s18b3_benua@179.ru>
2018-01-15 20:35:41 +0300

    parser Added

--- app/src/main/java/com/example/christian/cloudmessaging/ServerMessage.java
+++ app/src/main/java/com/example/christian/cloudmessaging/ServerMessage.java
@@ -1,12 +1,15 @@
 package com.example.christian.cloudmessaging;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.Gson;
 
 /**
  * Created by Christian on 15/01/18.
  */
 
-public class ServerMessage {
+public class ServerMessage implements Parcelable {
     public int getType() {
         return type;
     }
@@ -54,6 +57,12 @@ public class ServerMessage {
         this.cmd = a.cmd;
     }
 
+    public ServerMessage(Parcel in) {
+        type = in.readInt();
+        cmd = in.readInt();
+        uid = in.readInt();
+        data = in.readString();
+    }
 
     public ServerMessage(int type, int cmd, int uid, String data) {
         this.type = type;
@@ -67,5 +76,30 @@ public class ServerMessage {
         Gson gson = new Gson();
         return gson.fromJson(jsonQuery, ServerMessage.class);
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(type);
+        parcel.writeInt(cmd);
+        parcel.writeInt(uid);
+        parcel.writeString(data);
+    }
+
+    public static final Parcelable.Creator<ServerMessage> CREATOR = new Parcelable.Creator<ServerMessage>() {
+        @Override
+        public ServerMessage[] newArray(int i) {
+            return new ServerMessage[i];
+        }
+
+        @Override
+        public ServerMessage createFromParcel(Parcel parcel) {
+            return new ServerMessage(parcel);
+        }
+    };
 
 }
